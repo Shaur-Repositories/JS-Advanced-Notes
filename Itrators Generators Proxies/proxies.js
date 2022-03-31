53a6beb6dc6309ffa4155031ec3b8d4ee15b95a2
shaur <shaurya.jain.email@gmail.com>
2022-03-31 17:50:12 +0530

    proxy added

--- Itrators Generators Proxies/proxies.js
+++ Itrators Generators Proxies/proxies.js
@@ -1,5 +1,8 @@
 /*
 proxies in js is a way to wrap an object/ function and add a custum behaviour to it like reading, insertion or vaidation
+handler.get() --- for objects  --- The get() is fired when a property of the target object is accessed via the proxy object.
+handler.set() --- for objects ----  The set() controls behavior when a property of the target object is set.
+handler.apply() --- for functions  is for a function call.
 */
 
 console.log("-----------Proxies Examples----------------");
@@ -9,27 +12,51 @@ var testObj = {
     key2: 2,
 }
 console.log("-----------Proxies Get Handler Examples----------------");
+
+
+
 //get funciton as handler
 var getfun = {
-    get: function(obj, key , val){
-        obj[key] ? console.log("true") : console.log("false");
-        // obj[key] = val; // will act as set
+   get:function(obj, key , val){ //works like this or get(){} no other name will work
+        obj[key] ? console.log("true") : console.log("false");  // checks if key is present of not
+        obj[key] = val; // will act as set
     }
 }
 
 var proxy1 = new Proxy(testObj, getfun) // syntax for creating proxies  new Proxy(target obj/function , handler)
-// var proxy1 = new Proxy(testObj, {});// if handler part is left empty the proxy will work as the handler itself
+// var proxy1 = new Proxy(testObj, {});// if handler part is left empty the proxy will work as the handler(i.e. object) itself
 proxy1.key1
 // proxy1.key3 = 10; //get_set testing code.
+// console.log(testObj)// key3=10 will be added
 
 console.log("-----------Proxies Set Handler Examples----------------");
 //set function as handler
 var setfun = {
-    set: function (obj, key, val) {
+    set : function (obj, key, val) { // can also be used as set()
         obj[key] = val;
         console.log(obj)
     }
 }
-
 var proxy1 = new Proxy(testObj, setfun);
 proxy1.abc = 134;
+
+console.log("-----------Proxies Apply Handler Examples----------------");
+
+//apply handler function is used in fuction.
+
+var obj = {
+    param1: "abc",
+    param2: "xyz"
+}
+function test(anObj) {
+    return `${anObj.param1} ${anObj.param2}`
+}
+
+var handler = {
+    apply(fun, thisArg, param) { //thisArg is a part of syntax , fun is the "target function" and params is the "array of args pass in that function".
+        console.log(fun(...param).toUpperCase()) // just make the returned value to uppercase.
+    }
+};
+
+var funProxy = new Proxy(test, handler);
+funProxy(obj)
